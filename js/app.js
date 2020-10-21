6f6808da4173cb20b3a8d22516f8b857a637e623
francoandes12 <francoande12@gmail.com>
2020-10-21 17:57:17 -0300

    primer commit

---
+++ js/app.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+  cargarPortafolio()
+})
+
+const cargarPortafolio = () => {
+  fetch('datos.json')
+    .then((resp) => {
+      return resp.json()
+    })
+    .then((datos) => {
+      let html = ''
+      datos.amigurumis.forEach((amigurumis) => {
+        //crear el template
+        html += `
+        <div class="col-12 col-md-4 mb-2"
+        data-aos="zoom-in"
+        data-aos-duration="1000">
+        <div class="card">
+        <div class="card-body">
+        <p class="card-text text-center">Articulo ${amigurumis.id}</p>
+        </div>
+        <img
+            id="foto21"
+            loading="lazy"
+            class="img-fluid"
+            src="assets/img/${amigurumis.id}.jpeg"
+            alt="${amigurumis.nombre}"
+        />
+        <div class="card-body">
+            <p class="card-text text-center">${amigurumis.nombre}</p>
+        </div>
+        </div>
+        </div>
+        
+             `
+      })
+      //agregar al html
+      const contenedor = document.getElementById('tarjeta')
+      contenedor.innerHTML = html
+    })
+}
